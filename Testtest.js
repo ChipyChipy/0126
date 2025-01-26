f449d37544f0480b8342ef4d305c1868dc063b5e
ChipyChipy <156533218+ChipyChipy@users.noreply.github.com>
2025-01-26 10:52:27 +0800

    Update Testtest.js

--- Testtest.js
+++ Testtest.js
@@ -1,15 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // 檢測是否為行動裝置
-    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+function isMobileDevice() {
+    return /Mobi|Android|iPhone/i.test(navigator.userAgent);
+}
 
-    // 遍歷所有的超連結
-    document.querySelectorAll(".dynamic-link").forEach(link => {
-        if (isMobile) {
-            // 行動裝置：直接打開連結
-            link.setAttribute("target", "_self");
-        } else {
-            // 桌面設備：新分頁打開連結
-            link.setAttribute("target", "_blank");
-        }
-    });
+const links = document.querySelectorAll('.dynamic-link');
+
+links.forEach(link => {
+    if (!isMobileDevice()) {
+        link.setAttribute('target', '_blank');
+    } else {
+        link.removeAttribute('target');
+    }
 });
